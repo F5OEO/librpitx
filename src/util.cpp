7e50948296193c577c09011d8a8988933b4f00fe
F5OEO <evaristec@gmail.com>
2019-01-08 14:07:01 +0000

    Fix util.cpp missing

---
+++ src/util.cpp
@@ -0,0 +1,19 @@
+#include "util.h"
+
+static int debug_level = 0;
+
+void dbg_setlevel(int Level)
+{
+    debug_level=Level;
+}
+
+void dbg_printf(int Level, const char *fmt, ...)
+{
+    if (Level <= debug_level)
+    {
+        va_list args;
+        va_start(args, fmt);
+        vfprintf(stderr, fmt, args);
+        va_end(args);
+    }
+}
